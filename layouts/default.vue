0e9ab60f1979bbbb0ecef6773d7a2b2b311f2ec1
sabelogumede <iamsabelogumede@gmail.com>
2019-04-22 10:51:50 +0200

    project folder

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+
+    <TheNavbar/>
+    <nuxt/>
+    
+  </div>
+</template>
+
+<script>
+  import TheNavbar from '@/components/TheNavbar'
+
+  export default {
+    components: {
+      TheNavbar
+    }
+  };
+
+</script>
+
+
+<style>
+  .button--green {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
+
+  .button--green:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
+
+  .button--grey {
+    display: inline-block;
+    border-radius: 4px;
+
+  }
+
+</style>
---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all: [
+        {
+            id: 'balut',
+            title: 'What is Balut?',
+            content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a'
+        },
+        {
+            id: 'whereIsIt',
+            title: 'Where is the sign in button?',
+            content: 'I\'ve someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a'
+        },
+        {
+            id: 'how',
+            title: 'How can I fix this problem?',
+            content: 'Please help me. someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a'
+        }
+    ]
+})
